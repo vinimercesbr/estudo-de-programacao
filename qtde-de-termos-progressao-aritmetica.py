8dbcadafe4d14bd8eaa76888e004b4da99a397c5
Vinícius Bruno <vinimerces2016@gmail.com>
2025-04-13 10:21:21 -0300

    adicionar scripts para cálculo de termos e soma em progressão aritmética

---
+++ qtde-de-termos-progressao-aritmetica.py
@@ -0,0 +1,17 @@
+#Para determinar a quantidade de termos em uma progressão aritmética, usamos a seguinte fórmula:
+
+#n = (an - a1) / r + 1
+
+#Onde:
+#- n é o número total de termos da progressão,
+#- a1 é o primeiro termo,
+#- an é o último termo,
+#- r é a razão da progressão.
+
+#Exemplo:
+#Se temos uma progressão aritmética com primeiro termo a₁ = 2, último termo aₙ = 14 e razão r = 2, então:
+
+#n = (14 - 2) / 2 + 1 
+#n = 4
+
+#Portanto, essa progressão tem 4 termos.
---
+++ soma-progressao-aritmetica.py
@@ -0,0 +1,18 @@
+#Soma dos números de 2 até 14:
+
+#A soma dos números de 2 até 14 pode ser calculada usando a fórmula da soma de uma progressão aritmética:
+
+#n = (n / 2) * (a1 + an)
+
+#Onde:
+#- a1 = 2 (primeiro termo)
+#- an = 14 (último termo)
+#- n = 14 - 2 + 1 = 13 (quantidade de termos)
+
+#Substituindo os valores:
+
+#Sn = (13 / 2) * (2 + 14) 
+#Sn= (13 / 2) * 16 
+#Sn= 13 * 8 = 104
+
+#Portanto, a soma dos números de 2 até 14 é 104.
