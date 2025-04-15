25efa2ee0ddffb37d4c7fc1be5430b45acac0b43
Vinícius Bruno <vinimerces2016@gmail.com>
2025-04-15 00:35:14 +0000

    add files

---
+++ Exercícios Progressão aritmética/ex1.py
@@ -0,0 +1,19 @@
+#Exercício 1
+
+#É dado um algoritmo que:
+#   Lê os valores para A (quantidade de vezes que o laço vai repetir) e B (um número que muda a cada repetição).
+#   O algoritmo vai somar os valores lidos para B até i atingir A.
+
+#Sabemos que:
+#   O primeiro valor lido para B foi 3 e o quinto número foi 11, seguindo a sequência: 
+#   3, 5,7, 9, 11...
+
+#Essa é uma PA de razão 2.
+
+#Quantas vezes o laço repetiu?
+#A = 5
+
+#Quais valores foram lidos para B?
+# 3 + 5 + 7 + 9 + 11 = ?
+
+#Resultado esperado: 35
