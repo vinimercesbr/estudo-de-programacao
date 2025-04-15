91188dbf6be6bfa7e7e60a0954c9779493eaa2ff
vinimerces2016@gmail.com <vinimerces2016@gmail.com>
2025-04-15 10:58:31 -0300

    resolvido o exercício 1

--- Exercícios Progressão aritmética/ex1.py
+++ Exercícios Progressão aritmética/ex1.py
@@ -4,13 +4,6 @@
 #   Lê os valores para A (quantidade de vezes que o laço vai repetir) e B (um número que muda a cada repetição).
 #   O algoritmo vai somar os valores lidos para B até i atingir A.
   
-#Sn = (n / 2) * (a1 + an)
-
-#Onde:
-#- a1 = 2 (primeiro termo)
-#- an = 14 (último termo)
-#- n = 14 - 2 + 1 = 13 (quantidade de termos)
-
 #Sabemos que:
 #   O primeiro valor lido para B foi 3 e o quinto número foi 11, seguindo a sequência: 
 #   3, 5,7, 9, 11...
@@ -28,9 +21,16 @@
 razao=2
 laco_repetidor=5
 valor_primeiro_termo=3
-contador=0
 lista_b = []
+soma_lista=0
+
 for i in range(laco_repetidor):
-    razao=razao+2
-    lista_b.append(razao)
-    print(f'Valor Razão: {razao}')
+    lista_b.append(valor_primeiro_termo)
+    valor_primeiro_termo=valor_primeiro_termo+razao
+    
+print(lista_b)
+
+for i in lista_b:
+  soma_lista=soma_lista+i
+  
+print(soma_lista)
