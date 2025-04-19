ad1532d3793a059221f9dd3161a361bf50860167
Vinícius Bruno <vinimerces2016@gmail.com>
2025-04-19 09:47:19 -0300

    php

---
+++ php/potencia-2.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Complete a função que recebe um inteiro não negativo como entrada e retorna uma lista de todas as potências de com o expoente variando de a ( inclusive ).
+
+Exemplos:
+
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+*/
+//Declarações das varíaveis
+$numero_inteiro=2;
+$potencias=[];
+//Processamento
+ 
+echo($numero_inteiro);
+
+?>
