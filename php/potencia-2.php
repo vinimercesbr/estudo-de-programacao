de305a619a6f468146fcdc0ceb7cb1a68600f5cf
vinimerces2016@gmail.com <vinimerces2016@gmail.com>
2025-04-20 16:24:09 -0300

    refatoração

--- php/potencia-2.php
+++ php/potencia-2.php
@@ -11,21 +11,17 @@ n = 1  ==> [1, 2]     # [2^0, 2^1]
 n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 */
 //Declarações das varíaveis
+
 $base=2;
 $numero_expoente=2;
-$lista_expoentes=[];
-$contador=0;
+$lista_expoentes=range(0,$numero_expoente);
 $lista_potencias=[];
+
 //Processamento;
-while ($contador <= $numero_expoente):
-    $lista_expoentes[$contador]=$contador;
-    $contador=$contador+1;
-endwhile;
-//print_r($lista_expoentes);
-$contador=0;
+
 foreach ($lista_expoentes as $expoente) {
-	$contador=$contador+1;
-	$lista_potencias[$contador]=$base**$expoente;
+	$lista_potencias[$expoente]=$base**$expoente;
 };
+
 print_r($lista_potencias);
 ?>
