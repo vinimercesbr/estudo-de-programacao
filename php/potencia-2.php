3b0ce68d29a7bff10d06765a0705c3f808d0d971
vinimerces2016@gmail.com <vinimerces2016@gmail.com>
2025-04-19 21:57:57 -0300

    primeiros passos na resolução do exercício

--- php/potencia-2.php
+++ php/potencia-2.php
@@ -1,6 +1,8 @@
 <?php
 /*
-Complete a função que recebe um inteiro não negativo como entrada e retorna uma lista de todas as potências de com o expoente variando de a ( inclusive ).
+Complete a função que recebe um inteiro não negativo como entrada e retorna uma
+lista de todas as potências de com o expoente variando de a ( inclusive ).n^0
+n^2.
 
 Exemplos:
 
@@ -11,8 +13,11 @@ n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 //Declarações das varíaveis
 $numero_inteiro=2;
 $potencias=[];
-//Processamento
- 
-echo($numero_inteiro);
-
+$contador=0;
+//Processamento;
+while ($contador <= $numero_inteiro):
+    $potencias[$contador]=$contador;
+    $contador=$contador+1;
+endwhile;
+print_r($potencias);
 ?>
