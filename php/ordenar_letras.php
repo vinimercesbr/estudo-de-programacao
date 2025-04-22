32d9b4ce4a8d5b885b3c795e27fdf166d1719ba2
Vinícius Bruno <vinimerces2016@gmail.com>
2025-04-22 07:49:56 -0300

    Added capitalization logic

--- php/ordenar_letras.php
+++ php/ordenar_letras.php
@@ -1,7 +1,9 @@
 <?php
 /*Você receberá uma lista de strings. Você deve classificá-lo em ordem alfabética (diferencia maiúsculas de minúsculas e com base nos valores ASCII dos caracteres) e, em seguida, retornar o primeiro valor.
 O valor retornado deve ser uma cadeia de caracteres e ter entre cada uma de suas letras."***"
-Você não deve remover ou adicionar elementos de/para a matriz.*/
+Você não deve remover ou adicionar elementos de/para a matriz.
+
+Sáida:[A***p***p***l***e]*/
 ?>
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
@@ -10,8 +12,8 @@ use Faker\Factory;
 
 $faker = Factory::create();
 
-$letras=($faker->text(8));
-$lista_letras=str_split($letras);
+echo $letras=($faker->name());
+$lista_letras=str_split(strtoupper($letras));
 
 print_r($lista_letras);
 ?>
