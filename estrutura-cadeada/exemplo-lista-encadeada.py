639103c8138030a8e89eb40994e2e49a67297e83
Vinícius Bruno <117596069+vinimercesbr@users.noreply.github.com>
2025-04-23 23:38:19 +0000

    Mudançã de nome e conceito para melhor clareza

---
+++ estrutura-cadeada/exemplo-lista-encadeada.py
@@ -0,0 +1,16 @@
+"""
+#Faixa Etária
+#Declaração de vriáveis
+idade = 0
+#Entrada de dados
+idade = int(input("Digite a idade: "))
+#Processamento
+if idade < 12:
+  print("Criança")
+elif idade < 18:
+  print("Adolescente")
+elif idade < 60:
+  print("Adulto")
+else:
+  print("Idoso")
+"""
