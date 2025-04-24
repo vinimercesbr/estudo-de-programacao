48333b3fa3449d62516632b87c19416216c8f3af
Vinícius Bruno <117596069+vinimercesbr@users.noreply.github.com>
2025-04-24 01:07:04 +0000

    Lista de exercícios para fazer

--- estrutura-encadeada/ex-1.py
+++ estrutura-encadeada/ex-1.py
@@ -1,14 +1,35 @@
-#Faixa Etária
-#Declaração de vriáveis
-idade = 0
-#Entrada de dados
-idade = int(input("Digite a idade: "))
+"""
+1. Classificação e Cálculo
+Início
+ A ← 6 + 3
+ B ← 4 * 2
+ N ← (A * 2 + B) / 2
+ Se (N < 10) Então
+ R ← N * 2
+ Escreva("Dobro de N é ", R)
+ SenãoSe (N >= 10 E N <= 20) Então
+ R ← N + 5
+ Escreva("N somado com 5 é ", R)
+ Senão
+ R ← N - 3
+ Escreva("N subtraído por 3 é ", R)
+ FimSe
+Fim
+"""
+#Declarando as variáveis
+numero_primeiro = 6 + 3
+numero_segundo = 4 * 2
+resultado = 0
 #Processamento
-if (idade < 12):
-  print("Criança")
-elif (idade >= 12 and idade < 18):
-  print("Adolescente")
-if (idade <= 59 and idade >= 18):
-  print("Adulto")
+formula = (numero_primeiro * 2 + numero_segundo) / 2
+print(formula)
+#Estrutura  de controle
+if (formula < 10):
+  resultado = (formula * 2)
+  print(f"Dobro da fórmula é: {resultado}")
+elif (formula >= 10 and formula <= 20):
+  resultado = formula + 5
+  print(f"A fórmula somada com 5 é: {resultado}")
 else:
-  print("Idoso")
+  resultado = formula - 3
+  print(f"A fórmula subtraida por 3 é: {resultado}")
