2ef84a788905a24bf39c7f04cd49de2d3bfe37cf
Vinícius Bruno <117596069+vinimercesbr@users.noreply.github.com>
2025-04-28 23:11:54 +0000

    Reescrita para melhor interpretação

--- entendendo-algoritmos_exerciocios/Exercício-1.php
+++ entendendo-algoritmos_exerciocios/Exercício-1.php
@@ -1,17 +1,24 @@
 <?php
-/*A fórmula matemática usada para determinar o índice do elemento do meio é:
+/*
+Conceito:
 
-  meio=(inicio+fim)/2
+    A pesquisa binária é um algoritmo eficiente utilizado para encontrar um elemento em uma lista ordenada. Ele funciona dividindo repetidamente a lista ao meio, comparando o elemento buscado com o valor do meio da lista. Se o elemento estiver presente, a pesquisa retorna sua posição; caso contrário, retorna None. Este método é conhecido por sua eficiência, utilizando a técnica de "dividir e conquistar" para reduzir o número de comparações necessárias.
+
+Fórmula matemática:
+    Se usa para determinar o índice do elemento do meio é:
+    
+        meio=(inicio+fim)/2
   
-onde:
+    onde:
 
-início é o índice do primeiro elemento da lista,
+        início é o índice do primeiro elemento da lista,
 
-fim é o índice do último elemento da lista.
+        fim é o índice do último elemento da lista.
 
-EXERCÍCIOS 1.1  que você tenha uma lista com 128 nomes e esteja fazendo uma pesquisa binária. Qual seria o número máximo de etapas que você levaria para encontrar o nome desejado?
+EXERCÍCIOS:
+    1.1  que você tenha uma lista com 128 nomes e esteja fazendo uma pesquisa binária. Qual seria o número máximo de etapas que você levaria para encontrar o nome desejado?
 
-1.2 Suponha que você duplique o tamanho da lista. Qual seria o número máximo de etapas agora?*/
+    1.2 Suponha que você duplique o tamanho da lista. Qual seria o número máximo de etapas agora?*/
 ?>
 <?php
 require_once 'vendor/autoload.php';
@@ -21,14 +28,14 @@ $faker = Faker\Factory::create();
 $lista_names=[];
 $contador=0;
 while($contador<128){
-  $lista_names[$contador]=$faker->name();
-  $contador=$contador+1; 
+    $lista_names[$contador]=$faker->name();
+    $contador=$contador+1; 
 };
 
 foreach($lista_names as $nome){
-  echo $nome;
-  echo "\n";
-  echo $nome[0];
-  echo "\n";
+    echo $nome;
+    echo "\n";
+    echo $nome[0];
+    echo "\n";
 }
 ?>
