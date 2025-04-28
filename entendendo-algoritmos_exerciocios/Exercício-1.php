d419ef44f5783d790b31b837c48f01059e6e59ce
Vinícius Bruno <117596069+vinimercesbr@users.noreply.github.com>
2025-04-28 22:24:50 +0000

    esqueleto do algoritmo aparentemente concluído

--- entendendo-algoritmos_exerciocios/Exercício-1.php
+++ entendendo-algoritmos_exerciocios/Exercício-1.php
@@ -1,11 +1,28 @@
-<?php /*EXERCÍCIOS 1.1  que você tenha uma lista com 128 nomes e esteja fazendo uma pesquisa binária. Qual seria o número máximo de etapas que você levaria para encontrar o nome desejado?
+<?php
+/*A fórmula matemática usada para determinar o índice do elemento do meio é:
+
+  meio=(inicio+fim)/2
+  
+onde:
+
+início é o índice do primeiro elemento da lista,
+
+fim é o índice do último elemento da lista.
+
+EXERCÍCIOS 1.1  que você tenha uma lista com 128 nomes e esteja fazendo uma pesquisa binária. Qual seria o número máximo de etapas que você levaria para encontrar o nome desejado?
+
 1.2 Suponha que você duplique o tamanho da lista. Qual seria o número máximo de etapas agora?*/
 ?>
 <?php
 require_once 'vendor/autoload.php';
 
-// use the factory to create a Faker\Generator instance
 $faker = Faker\Factory::create();
-// generate data by calling methods
-echo $faker->name();
+
+$lista_names=[];
+$contador=0;
+while($contador<128){
+  $lista_names[$contador]=$faker->name();
+  $contador=$contador+1; 
+};
+print_r($lista_names);
 ?>
