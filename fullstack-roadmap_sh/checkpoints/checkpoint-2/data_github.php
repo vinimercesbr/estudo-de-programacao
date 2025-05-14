47fc725c060dafadd36c1834c59fa95b971e43aa
Vinícius Bruno <117596069+vinimercesbr@users.noreply.github.com>
2025-05-14 22:25:43 +0000

    Comando de terminal a se usar

---
+++ fullstack-roadmap_sh/checkpoints/checkpoint-2/data_github.php
@@ -0,0 +1,10 @@
+<?php
+//https://roadmap.sh/full-stack
+//An application that optionally takes two dates and prints the most starred GitHub projects in that date range. Hint you can use GitHub's search API
+//Um aplicativo que, opcionalmente, recebe duas datas e exibe os projetos do GitHub com mais estrelas nesse intervalo de datas. Dica: você pode usar a API de pesquisa do GitHub.
+/*curl -L \
+-H "Accept: application/vnd.github+json" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+"https://api.github.com/search/repositories?q=!NOME DO REPOSITORIO&sort=stars&order=desc&per_page=5"
+*/
+?>
