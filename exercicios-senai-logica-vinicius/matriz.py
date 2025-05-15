6eb086689d6a36d9670657df5cfed445f928832b
Vinícius Bruno <117596069+vinimercesbr@users.noreply.github.com>
2025-05-15 23:32:02 +0000

    organização em uma única lista

--- exercicios-senai-logica-vinicius/matriz.py
+++ exercicios-senai-logica-vinicius/matriz.py
@@ -1,17 +1,16 @@
 #Escreva a matriz  acima com índices ao lado de cada número?// a[0][0]=2
-linha_primeira = [2, 3, 9, 18]
-linha_segunda = [4, 8, 12, 15]
-linha_terceira = [6, 7, 11, 13]
+
+linhas = [[2, 3, 9, 18], [4, 8, 12, 15], [6, 7, 11, 13]]
 
 
 def listas(lista, indice):
   contador = 0
   for index in lista:
-    print(indice, contador, index)
+    print(f'[{indice}] [{contador}] = {index}')
     contador = contador + 1
 
 
 indice = [0, 1, 2]
-listas(linha_primeira, indice[0])
-listas(linha_segunda, indice[1])
-listas(linha_terceira, indice[2])
+listas(linhas[0], indice[0])
+listas(linhas[1], indice[1])
+listas(linhas[2], indice[2])
