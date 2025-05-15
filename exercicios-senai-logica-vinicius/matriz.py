78fda650d42a12189808556d583e94d6ae0c0c5b
Vinícius Bruno <117596069+vinimercesbr@users.noreply.github.com>
2025-05-15 22:43:24 +0000

    resolvido o exercício

--- exercicios-senai-logica-vinicius/matriz.py
+++ exercicios-senai-logica-vinicius/matriz.py
@@ -4,4 +4,14 @@ linha_segunda = [4, 8, 12, 15]
 linha_terceira = [6, 7, 11, 13]
 
 
- 
+def listas(lista, indice):
+  contador = 0
+  for index in lista:
+    print(indice, contador, index)
+    contador = contador + 1
+
+
+indice = [0, 1, 2]
+listas(linha_primeira, indice[0])
+listas(linha_segunda, indice[1])
+listas(linha_terceira, indice[2])
