cce90cf9a2f7190ceef9fd23f14b5b5b7c0781ce
vinimercesbr <vinimerces2016.com>
2025-05-25 10:05:29 -0300

    remoção

--- development-roadmap-sh/backend-roadmap-sh/beginner/task-manager/task-manager.php
+++ development-roadmap-sh/backend-roadmap-sh/beginner/task-manager/task-manager.php
@@ -6,19 +6,16 @@ function criar_json($lista_tarefas){
   file_put_contents($file, $current);
 };
 
-function processamento_tarefas($tarefa,$nome_tarefa){
-
-  $lista_tarefas=array($nome_tarefa=>$tarefa);
+function processamento_tarefas($tarefa,){
+  $lista_tarefas=array("tarefa"=>$tarefa);
   $lista_tarefas=json_encode($lista_tarefas, JSON_PRETTY_PRINT);
   criar_json($lista_tarefas);
 };
 
 function criar_tarefa(){
-  echo "Digite o nome da tarefa: \n";
-  $nome_tarefa=trim(fgets(STDIN));
   echo "Digite sua tarefa: \n";
   $tarefa=trim(fgets(STDIN));
-  processamento_tarefas($tarefa,$nome_tarefa);
+  processamento_tarefas($tarefa);
 };
 /*function criar_id(){
   $id=bin2hex(random_bytes(8));
