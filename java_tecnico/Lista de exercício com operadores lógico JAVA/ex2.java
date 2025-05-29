de3286f2456341a2fe47ca0475f5a363acd27de2
vinimercesbr <vinimerces2016.com>
2025-05-29 15:49:48 -0300

    add

---
+++ java_tecnico/Lista de exercício com operadores lógico JAVA/ex2.java
@@ -0,0 +1,25 @@
+/*2. Crie um programa que pergunte se o usuário tem ingresso e se está com pulseira VIP. Ele só pode entrar na área VIP se tiver ingresso e estiver com pulseira VIP. */
+//usar boolean
+import java.util.Scanner;
+
+public class ex2 {
+    public static boolean verificar(boolean ingresso, boolean vip){
+        if((vip==true)&&(ingresso==true)){
+            System.out.println("Você pode entrar na área vip");
+            return true;
+            }else{
+                System.out.println("Você está proibido de entrar na área vip");
+            return false;
+
+            }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Você tem ingresso?(true,false)");
+        boolean ingresso = scanner.nextBoolean();
+        System.out.println("Você está com  pulseira vip?(true,false)");
+        boolean vip = scanner.nextBoolean();
+        scanner.close();
+        boolean retorno=verificar(ingresso,vip);
+    }
+}
