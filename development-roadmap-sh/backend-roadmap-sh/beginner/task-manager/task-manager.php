b2eafa7d937cec84c5996289770519988ff1e0e7
vinimercesbr <vinimerces2016.com>
2025-05-31 20:39:01 -0300

    erros no array

--- development-roadmap-sh/backend-roadmap-sh/beginner/task-manager/task-manager.php
+++ development-roadmap-sh/backend-roadmap-sh/beginner/task-manager/task-manager.php
@@ -1,18 +1,20 @@
 <?php
 function check_file($file,$list_task){
   $check=file_exists($file);
-  if($check==false){
-    file_put_contents($file,$list_task);
-    $list_task=json_encode($list_task, JSON_PRETTY_PRINT);
-  }else{
-    $cint
-    $list_task=json_decode($list_task, JSON_PRETTY_PRINT);
-    $current = file_get_contents($file);
-    $current .= $list_task;
-    file_put_contents($file, $current);
-    $list_task=json_encode($list_task, JSON_PRETTY_PRINT);
+  foreach ($list_task as $index_list_task){
+    if($check==false){
+      file_put_contents($file,$index_list_task);
+      $list_task_json=json_encode($index_list_task, JSON_PRETTY_PRINT);
+    }else{
+      $list_task_json=json_decode($index_list_task, JSON_PRETTY_PRINT);
+      $current = file_get_contents($file);
+      $current .=$index_list_task;
+      file_put_contents($file, $current);
+      $list_task_json=json_encode($index_list_task, JSON_PRETTY_PRINT);
+    };
   };
 };
+
 function create_json($list_task){
   $file = 'list_task.json';
   check_file($file,$list_task);
