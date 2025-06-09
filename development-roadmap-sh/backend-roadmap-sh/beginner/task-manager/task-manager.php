d441f7f20237c9a2463fd9fba02b9e1ca012dc6f
Vinícius Bruno <vinimerces2016@gmail.com>
2025-06-09 09:40:31 -0300

    Function to create array index completed

--- development-roadmap-sh/backend-roadmap-sh/beginner/task-manager/task-manager.php
+++ development-roadmap-sh/backend-roadmap-sh/beginner/task-manager/task-manager.php
@@ -6,20 +6,31 @@ function check_file($file,$list_task){
       file_put_contents($file,$list_task_json);
     return true;
     }else{
-      foreach ($list_task as $index){
-        print_r($index);
-      }
+      
     }
 }
 
 function write_tasks_to_json_file($tasks){
   $file = 'list_task.json';
+  print_r($tasks);
   check_file($file, $tasks);
 }
 
+function contador_geral($contador_tasks){
+  static $contador=-1;
+  while($contador<$contador_tasks){
+    $contador=$contador+1;
+    echo $contador;
+    echo "\n";
+  }
+  return $contador;
+}
+
 function save_task_to_json($task){
-  $tasks = array($task);
-  write_tasks_to_json_file($tasks);
+  static $contador=0;
+  $contador=contador_geral(90);
+  echo $contador;
+  //write_tasks_to_json_file($tasks);
 }
 
 function prompt_user_for_task(){
